9a77b1ee5a4f6babe30944fd7afb5c88a92b81b7
Freddy Brasileiro <freddybrasileiro@users.noreply.github.com>
2015-07-09 16:54:34 -0300

    Adjusting OOTOS

---
+++ net.menthor.common/src/net/menthor/common/transformation/owl/OWLMappingTypes.java
@@ -0,0 +1,5 @@
+package net.menthor.common.transformation.owl;
+
+public enum OWLMappingTypes {
+	SIMPLE, WORM_VIEW_A0, WORM_VIEW_A1, WORM_VIEW_A2, REIFICATION, OOTOS, UFO_RDF
+}
---
+++ net.menthor.common/src/net/menthor/common/transformation/owl/OWLTransformationOptions.java
@@ -0,0 +1,205 @@
+package net.menthor.common.transformation.owl;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import net.menthor.common.transformation.TransformationOptions;
+
+import org.eclipse.emf.ecore.EObject;
+
+import RefOntoUML.util.OntoUMLElement;
+
+
+public class OWLTransformationOptions extends TransformationOptions {
+	String ontologyIri = "";
+	OWLMappingTypes mappingType;
+	boolean generateFile = false;
+	String filePath = "";
+	
+	//disjointness
+	boolean disjointClassAxioms = true;
+	boolean disjointObjectPropertyAxioms = true;
+	boolean disjointAssociationAxioms = true;
+	boolean completeClassAxiom = true;
+	
+	//associations
+	boolean domainAxiom = true;
+	boolean rangeAxiom = true;
+	boolean inverseAxiom = true;
+	
+	//association binary association
+	boolean reflexiveAxiom = true;
+	boolean irreflexiveAxiom = true;
+	boolean symmetricAxiom = true;
+	boolean asymmetricAxiom = true;
+	boolean transitiveAxiom = true;
+	boolean functionalAxiom = true;
+	boolean inverseFunctionalAxiom = true;
+	
+	//swrl rules
+	boolean swrlRulesAxiom = true;
+	
+	//cardinality
+	boolean cardinalityAxiom = true;
+	
+	boolean ufoStructure = false;
+	
+	HashMap<OntoUMLElement, Object> primitiveTypeMappingsOntoUMLElement = new HashMap<OntoUMLElement, Object>();
+	HashMap<OntoUMLElement, Object> attributeMappingsOntoUMLElement = new HashMap<OntoUMLElement, Object>();
+	HashMap<EObject, Object> primitiveTypeMappingsEObject = new HashMap<EObject, Object>();
+	HashMap<EObject, Object> attributeMappingsEObject = new HashMap<EObject, Object>();
+	
+	public HashMap<OntoUMLElement, Object> getAttributeMappingsOntoUMLElement() {
+		return attributeMappingsOntoUMLElement;
+	}
+	public HashMap<OntoUMLElement, Object> getPrimitiveTypeMappingsOntoUMLElement() {
+		return primitiveTypeMappingsOntoUMLElement;
+	}
+	public void setPrimitiveTypeMappingsOntoUMLElement(HashMap<OntoUMLElement, Object> primitiveTypeMappingsOntoUMLElement) {
+		this.primitiveTypeMappingsOntoUMLElement = primitiveTypeMappingsOntoUMLElement;
+		
+		for (Entry<OntoUMLElement, Object> entry : primitiveTypeMappingsOntoUMLElement.entrySet()) {
+			primitiveTypeMappingsEObject.put(entry.getKey().getElement(),entry.getValue());
+		}
+	}
+	public void setAttributeMappingsOntoUMLElement(HashMap<OntoUMLElement, Object> attributeMappingsOntoUMLElement) {
+		this.attributeMappingsOntoUMLElement = attributeMappingsOntoUMLElement;
+
+		for (Entry<OntoUMLElement, Object> entry : attributeMappingsOntoUMLElement.entrySet()) {
+			attributeMappingsEObject.put(entry.getKey().getElement(),entry.getValue());
+		}
+	}	
+	public HashMap<EObject, Object> getAttributeMappingsEObject() {
+		return attributeMappingsEObject;
+	}
+	public HashMap<EObject, Object> getPrimitiveTypeMappingsEObject() {
+		return primitiveTypeMappingsEObject;
+	}
+	public boolean isCompleteClassAxiom() {
+		return completeClassAxiom;
+	}
+	public void setCompleteClassAxiom(boolean completeClassAxiom) {
+		this.completeClassAxiom = completeClassAxiom;
+	}
+	public boolean isDisjointObjectPropertyAxioms() {
+		return disjointObjectPropertyAxioms;
+	}
+	public void setDisjointObjectPropertyAxioms(boolean disjointObjectPropertyAxioms) {
+		this.disjointObjectPropertyAxioms = disjointObjectPropertyAxioms;
+	}
+	public OWLMappingTypes getMappingType() {
+		return mappingType;
+	}
+	public void setMappingType(OWLMappingTypes mappingType) {
+		this.mappingType = mappingType;
+	}
+	public boolean isDisjointClassAxioms() {
+		return disjointClassAxioms;
+	}
+	public void setDisjointClassAxioms(boolean disjointClassAxioms) {
+		this.disjointClassAxioms = disjointClassAxioms;
+	}
+	public boolean isDisjointAssociationAxioms() {
+		return disjointAssociationAxioms;
+	}
+	public void setDisjointAssociationAxioms(boolean disjointAssociationAxioms) {
+		this.disjointAssociationAxioms = disjointAssociationAxioms;
+	}
+	public boolean isDomainAxiom() {
+		return domainAxiom;
+	}
+	public void setDomainAxiom(boolean domainAxiom) {
+		this.domainAxiom = domainAxiom;
+	}
+	public boolean isRangeAxiom() {
+		return rangeAxiom;
+	}
+	public void setRangeAxiom(boolean rangeAxiom) {
+		this.rangeAxiom = rangeAxiom;
+	}
+	public boolean isInverseAxiom() {
+		return inverseAxiom;
+	}
+	public void setInverseAxiom(boolean inverseAxiom) {
+		this.inverseAxiom = inverseAxiom;
+	}
+	public boolean isReflexiveAxiom() {
+		return reflexiveAxiom;
+	}
+	public void setReflexiveAxiom(boolean reflexiveAxiom) {
+		this.reflexiveAxiom = reflexiveAxiom;
+	}
+	public boolean isIrreflexiveAxiom() {
+		return irreflexiveAxiom;
+	}
+	public void setIrreflexiveAxiom(boolean irreflexiveAxiom) {
+		this.irreflexiveAxiom = irreflexiveAxiom;
+	}
+	public boolean isSymmetricAxiom() {
+		return symmetricAxiom;
+	}
+	public void setSymmetricAxiom(boolean symmetricAxiom) {
+		this.symmetricAxiom = symmetricAxiom;
+	}
+	public boolean isAsymmetricAxiom() {
+		return asymmetricAxiom;
+	}
+	public void setAsymmetricAxiom(boolean asymmetricreflexiveAxiom) {
+		this.asymmetricAxiom = asymmetricreflexiveAxiom;
+	}
+	public boolean isTransitiveAxiom() {
+		return transitiveAxiom;
+	}
+	public void setTransitiveAxiom(boolean transitiveAxiom) {
+		this.transitiveAxiom = transitiveAxiom;
+	}
+	public boolean isFunctionalAxiom() {
+		return functionalAxiom;
+	}
+	public void setFunctionalAxiom(boolean functionalAxiom) {
+		this.functionalAxiom = functionalAxiom;
+	}
+	public boolean isInverseFunctionalAxiom() {
+		return inverseFunctionalAxiom;
+	}
+	public void setInverseFunctionalAxiom(boolean inverseFunctionalAxiom) {
+		this.inverseFunctionalAxiom = inverseFunctionalAxiom;
+	}
+	public String getOntologyIri() {
+		return ontologyIri;
+	}
+	public void setOntologyIri(String ontologyIri) {
+		this.ontologyIri = ontologyIri;
+	}
+	public boolean isGenerateFile() {
+		return generateFile;
+	}
+	public void setGenerateFile(boolean generateFile) {
+		this.generateFile = generateFile;
+	}
+	public String getFilePath() {
+		return filePath;
+	}
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	public void setCardinalityAxiom(boolean cardinality) {
+		this.cardinalityAxiom = cardinality;		
+	}
+	public boolean isSwrlRulesAxiom() {
+		return swrlRulesAxiom;
+	}
+	public void setSwrlRulesAxiom(boolean swrlRulesAxiom) {
+		this.swrlRulesAxiom = swrlRulesAxiom;
+	}
+	public boolean isCardinalityAxiom() {
+		return cardinalityAxiom;
+	}
+	public boolean isUfoStructure() {
+		return ufoStructure;
+	}
+	public void setUfoStructure(boolean ufoStructure) {
+		this.ufoStructure = ufoStructure;
+	}
+	
+}
