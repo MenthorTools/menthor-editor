cfa90702085c100dfbec6f3b6e0e3f70bed62f68
Bernardo F B Braga <bernardofbbraga@gmail.com>
2015-08-25 18:08:13 -0300

    Save/Load now works
    Legacy files may have to be updated

--- net.menthor.story/src/stories/impl/StoryImpl.java
+++ net.menthor.story/src/stories/impl/StoryImpl.java
@@ -229,7 +229,7 @@ public class StoryImpl extends MinimalEObjectImpl.Container implements Story {
 		for(Story_element se : this.getElements()){
 			if("Node".equals(se.eClass().getName())){
 				System.out.println();
-				head = head+'\n'+'\t'+'\t'+'\t'+'\t'+'\t'+ se.getLabel()+":one Object+Property,";
+				head = head+'\n'+'\t'+'\t'+'\t'+'\t'+'\t'+ se.getLabel()+":one univ-World,";
 			}else if("World".equals(se.eClass().getName())){
 				head = head+'\n'+'\t'+'\t'+'\t'+'\t'+'\t'+ se.getLabel()+":one World,";
 			}
