c875a2c8d3f6b71ea3e73c5c4086c3d3f8fda6a2
Petr Kremen <kremep1@fel.cvut.cz>
2017-01-05 11:49:29 +0100

    [UPD] make the code of DataTypeMap declarative

--- net.menthor.ontouml2simpleowl/src/net/menthor/ontouml2simpleowl/transform/DatatypeMap.java
+++ net.menthor.ontouml2simpleowl/src/net/menthor/ontouml2simpleowl/transform/DatatypeMap.java
@@ -1,97 +1,84 @@
 package net.menthor.ontouml2simpleowl.transform;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLDatatype;
 import org.semanticweb.owlapi.vocab.OWL2Datatype;
 
 public class DatatypeMap {
 
+	private final static Map<String,IRI> builtInDataTypeMap = new HashMap<>();
+	private final static Map<String,IRI> dataTypeMap = new HashMap<>();
+	
+	static {		
+		// int => INT => INTEGER
+		// datetime => XSD_DATE_TIME => XSD_DATE_TIME_STAMP
+		// dateTime => XSD_DATE_TIME_STAMP
+		
+		builtInDataTypeMap.put("string", OWL2Datatype.XSD_STRING.getIRI());
+		builtInDataTypeMap.put("int", OWL2Datatype.XSD_INT.getIRI());
+		builtInDataTypeMap.put("integer", OWL2Datatype.XSD_INTEGER.getIRI());
+		builtInDataTypeMap.put("boolean", OWL2Datatype.XSD_BOOLEAN.getIRI());
+		builtInDataTypeMap.put("datetime", OWL2Datatype.XSD_DATE_TIME.getIRI());
+		builtInDataTypeMap.put("date", OWL2Datatype.XSD_DATE_TIME.getIRI());
+		builtInDataTypeMap.put("long", OWL2Datatype.XSD_LONG.getIRI());
+		builtInDataTypeMap.put("float", OWL2Datatype.XSD_FLOAT.getIRI());
+		builtInDataTypeMap.put("byte", OWL2Datatype.XSD_BYTE.getIRI());
+		
+		dataTypeMap.put("unsigned_int", OWL2Datatype.XSD_UNSIGNED_INT.getIRI());
+		dataTypeMap.put("unsignedInt", OWL2Datatype.XSD_UNSIGNED_INT.getIRI());
+		dataTypeMap.put("IntegerIntervalDimension", OWL2Datatype.XSD_INTEGER.getIRI());
+		dataTypeMap.put("IntegerOrdinalDimension", OWL2Datatype.XSD_INTEGER.getIRI());
+		dataTypeMap.put("IntegerRationalDimension", OWL2Datatype.XSD_INTEGER.getIRI());
+		dataTypeMap.put("unsigned_byte", OWL2Datatype.XSD_UNSIGNED_BYTE.getIRI());
+		dataTypeMap.put("unsignedByte", OWL2Datatype.XSD_UNSIGNED_BYTE.getIRI());
+		dataTypeMap.put("double", OWL2Datatype.XSD_DOUBLE.getIRI());
+		dataTypeMap.put("NominalQuality", OWL2Datatype.XSD_STRING.getIRI());
+		dataTypeMap.put("normalized_string", OWL2Datatype.XSD_NORMALIZED_STRING.getIRI());
+		dataTypeMap.put("normalizedString", OWL2Datatype.XSD_NORMALIZED_STRING.getIRI());
+		dataTypeMap.put("hex_binary", OWL2Datatype.XSD_HEX_BINARY.getIRI());
+		dataTypeMap.put("hexBinary", OWL2Datatype.XSD_HEX_BINARY.getIRI());
+		dataTypeMap.put("short", OWL2Datatype.XSD_SHORT.getIRI());
+		dataTypeMap.put("unsigned_long", OWL2Datatype.XSD_UNSIGNED_LONG.getIRI());
+		dataTypeMap.put("unsignedLong", OWL2Datatype.XSD_UNSIGNED_LONG.getIRI());
+		dataTypeMap.put("anyURI", OWL2Datatype.XSD_ANY_URI.getIRI());
+		dataTypeMap.put("base64Binary", OWL2Datatype.XSD_BASE_64_BINARY.getIRI());
+		dataTypeMap.put("decimal", OWL2Datatype.XSD_DECIMAL.getIRI());
+		dataTypeMap.put("DecimalIntervalDimension", OWL2Datatype.XSD_DECIMAL.getIRI());
+		dataTypeMap.put("DecimalOrdinalDimension", OWL2Datatype.XSD_DECIMAL.getIRI());
+		dataTypeMap.put("DecimalRationalDimension", OWL2Datatype.XSD_DECIMAL.getIRI());
+		dataTypeMap.put("Name", OWL2Datatype.XSD_NAME.getIRI());
+		dataTypeMap.put("NCName", OWL2Datatype.XSD_NCNAME.getIRI());
+		dataTypeMap.put("nonPositiveInteger", OWL2Datatype.XSD_NON_POSITIVE_INTEGER.getIRI());
+		dataTypeMap.put("nonNegativeInteger", OWL2Datatype.XSD_NON_NEGATIVE_INTEGER.getIRI());
+		dataTypeMap.put("unsignedShort", OWL2Datatype.XSD_UNSIGNED_SHORT.getIRI());
+		dataTypeMap.put("negativeInteger", OWL2Datatype.XSD_NEGATIVE_INTEGER.getIRI());
+		dataTypeMap.put("positiveInteger", OWL2Datatype.XSD_POSITIVE_INTEGER.getIRI());
+		dataTypeMap.put("language", OWL2Datatype.XSD_LANGUAGE.getIRI());
+		dataTypeMap.put("token", OWL2Datatype.XSD_TOKEN.getIRI());
+		dataTypeMap.put("NMTOKEN", OWL2Datatype.XSD_NMTOKEN.getIRI());
+	}
+	
 	private static OWLDataFactory f() {
 		return OWLManager.getOWLDataFactory();
 	}
 
-	public static OWLDatatype getBuiltinType(String name) {
-		if (name.equalsIgnoreCase("string")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_STRING.getIRI());
-		} else if (name.equalsIgnoreCase("int")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_INT.getIRI());
-		} else if (name.equalsIgnoreCase("integer")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_INTEGER.getIRI());
-		} else if (name.equalsIgnoreCase("boolean")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_BOOLEAN.getIRI());
-		} else if (name.equalsIgnoreCase("datetime") || name.equalsIgnoreCase("date")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_DATE_TIME.getIRI());
-		} else if (name.equalsIgnoreCase("long")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_LONG.getIRI());
-		} else if (name.equalsIgnoreCase("float")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_FLOAT.getIRI());
-		} else if (name.equalsIgnoreCase("byte")) {
-			return f().getOWLDatatype(OWL2Datatype.XSD_BYTE.getIRI());
-		}
 
-		return null;
+	public static OWLDatatype getBuiltinType(String name) {
+		return f().getOWLDatatype(builtInDataTypeMap.get(name));
 	}
 	
 	public static OWLDatatype getType(String name) {
-		if (name.equalsIgnoreCase("unsigned_int") || name.equalsIgnoreCase("unsignedInt")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_UNSIGNED_INT.getIRI());
-		}else if(name.equalsIgnoreCase("int") || name.equalsIgnoreCase("integer") || name.equalsIgnoreCase("IntegerIntervalDimension") || name.equalsIgnoreCase("IntegerOrdinalDimension") || name.equalsIgnoreCase("IntegerRationalDimension")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_INTEGER.getIRI());
-		}else if(name.equalsIgnoreCase("unsigned_byte") || name.equalsIgnoreCase("unsignedByte")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_UNSIGNED_BYTE.getIRI());
-		}else if(name.equalsIgnoreCase("double")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_DOUBLE.getIRI());
-		}else if(name.equalsIgnoreCase("string") || name.equalsIgnoreCase("NominalQuality")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_STRING.getIRI());
-		}else if(name.equalsIgnoreCase("normalized_string") || name.equalsIgnoreCase("normalizedString")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NORMALIZED_STRING.getIRI());
-		}else if(name.equalsIgnoreCase("boolean")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_BOOLEAN.getIRI());
-		}else if(name.equalsIgnoreCase("hex_binary") || name.equalsIgnoreCase("hexBinary")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_HEX_BINARY.getIRI());
-		}else if(name.equalsIgnoreCase("short")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_SHORT.getIRI());
-		}else if(name.equalsIgnoreCase("byte")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_BYTE.getIRI());
-		}else if(name.equalsIgnoreCase("unsigned_long") || name.equalsIgnoreCase("unsignedLong")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_UNSIGNED_LONG.getIRI());
-		}else if(name.equalsIgnoreCase("anyURI")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_ANY_URI.getIRI());
-		}else if(name.equalsIgnoreCase("base64Binary")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_BASE_64_BINARY.getIRI());
-		}else if(name.equalsIgnoreCase("date")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_DATE_TIME.getIRI());
-		}else if(name.equalsIgnoreCase("dateTime")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_DATE_TIME_STAMP.getIRI());
-		}else if(name.equalsIgnoreCase("decimal") || name.equalsIgnoreCase("DecimalIntervalDimension") || name.equalsIgnoreCase("DecimalOrdinalDimension") || name.equalsIgnoreCase("DecimalRationalDimension")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_DECIMAL.getIRI());
-		}else if(name.equalsIgnoreCase("Name")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NAME.getIRI());
-		}else if(name.equalsIgnoreCase("NCName")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NCNAME.getIRI());
-		}else if(name.equalsIgnoreCase("nonPositiveInteger")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NON_POSITIVE_INTEGER.getIRI());
-		}else if(name.equalsIgnoreCase("nonNegativeInteger")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NON_NEGATIVE_INTEGER.getIRI());
-		}else if(name.equalsIgnoreCase("unsignedShort")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_UNSIGNED_SHORT.getIRI());
-		}else if(name.equalsIgnoreCase("negativeInteger")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NEGATIVE_INTEGER.getIRI());
-		}else if(name.equalsIgnoreCase("positiveInteger")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_POSITIVE_INTEGER.getIRI());
-		}else if(name.equalsIgnoreCase("language")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_LANGUAGE.getIRI());
-		}else if(name.equalsIgnoreCase("long")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_LONG.getIRI());
-		}else if(name.equalsIgnoreCase("float")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_FLOAT.getIRI());
-		}else if(name.equalsIgnoreCase("token")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_TOKEN.getIRI());
-		}else if(name.equalsIgnoreCase("NMTOKEN")){
-			return f().getOWLDatatype(OWL2Datatype.XSD_NMTOKEN.getIRI());
+		if (builtInDataTypeMap.containsKey(name)) {
+			return f().getOWLDatatype(builtInDataTypeMap.get(name));
 		}
-		
+		if (dataTypeMap.containsKey(name)) {
+			return f().getOWLDatatype(dataTypeMap.get(name));
+		}
 		return f().getOWLDatatype(OWL2Datatype.RDFS_LITERAL.getIRI());
 	}	
 }
